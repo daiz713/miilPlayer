eddb9464f92b3337aa2d5ec460ea8cb0c0738707
daiiz <da.iz.mg@gmail.com>
2015-11-27 17:26:04 +0900

    category内に含まれる写真一覧を取得

--- miilplayer/app/controllers/miilcategories_controller.rb
+++ miilplayer/app/controllers/miilcategories_controller.rb
@@ -1,9 +1,33 @@
+require 'open-uri'
+require './app/controllers/scan_miilcategories_photos.rb'
+
 class MiilcategoriesController < ApplicationController
 
+    # miilカテゴリの一覧を取得
     # GET /miilcategories
     # GET /miilcategories.json
     def index
         @categoryList = File.read(Rails.root.join('app').join('assets').join('javascripts').join('miilcategories.json'))
+        # クライントに返却
         render :json => @categoryList
+    end
+
+    # miilカテゴリに含まれる写真一覧を取得
+    # GET /miilcategories/722
+    # GET /miilcategories/722.json
+    def show
+        @categoryId = params[:id]
+        #res = open('http://miil.me/api/photos/recent/categories/722.json')
+        res = open('http://localhost:4567/miilcategories722');
+        code, message = res.status
+        if code == '200'
+            # DBに登録
+            #scan_miilcategories_photos(res.read)
+            # クライントに返却
+            render :json => {
+                'category_id' => @categoryId,
+                'category_photo' => (ActiveSupport::JSON.decode res.read)
+            }
+        end
     end
 end
