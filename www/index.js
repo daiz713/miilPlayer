c91cae0be92d661cc20f77047d3904d259d51293
daiiz <da.iz.mg@gmail.com>
2015-12-02 18:28:17 +0900

    Playerに表示する処理を関数化

--- www/index.js
+++ www/index.js
@@ -57,6 +57,30 @@ var MiilPlayer = (function () {
             this.menuBar.show();
             this.pagerBar.show();
         }
+    }, {
+        key: 'showItems',
+        value: function showItems(apiUrl) {
+            var self = this;
+            var photo_stock = [];
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', apiUrl, true);
+            xhr.responseType = 'json';
+            xhr.onload = function (e) {
+                var res = this.response;
+                var photos = res.photo.photos;
+                for (var i = 0; i < photos.length; i++) {
+                    var photo = photos[i].photo_url;
+                    var page = photos[i].page_url;
+                    photo_stock.push({ photo: photo, page: page });
+                }
+                // 初期collectionを登録
+                self.photoPanel.setCollection(photo_stock);
+                // 初期画像を表示
+                self.showNextItem();
+                self.startSlideShow();
+            };
+            xhr.send();
+        }
 
         // 自動スライドショーを開始する
     }, {
--- www/ui.js
+++ www/ui.js
@@ -11,25 +11,6 @@ $(function () {
     mp.menuBar.hide();
     mp.pagerBar.hide();
 
-    var photo_stock = [];
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/miilusers/daiz.json', true);
-    xhr.responseType = 'json';
-    xhr.onload = function(e) {
-        var res = this.response;
-        var photos = res.photo.photos;
-        for(var i = 0; i < photos.length; i++) {
-            var photo = photos[i].photo_url;
-            var page  = photos[i].page_url;
-            photo_stock.push({photo: photo, page: page});
-        }
-        // 初期collectionを登録
-        mp.photoPanel.setCollection(photo_stock);
-        // 初期画像を表示
-        mp.showNextItem();
-        mp.startSlideShow();
-    }
-    xhr.send();
-
+    mp.showItems('http://localhost:3000/miilusers/daiz.json');
     console.info('main ok');
 });
