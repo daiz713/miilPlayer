d932c0fea3e70f3a6bf2013365edcdfc424dc4a8
daiiz <da.iz.mg@gmail.com>
2015-12-04 13:39:39 +0900

    クラス書いてる（途中）

--- www/collections.js
+++ www/collections.js
@@ -1,18 +1,39 @@
-"use strict";
+// コレクションリストを管理するクラス
+'use strict';
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var PhotosStage = function PhotosStage($dom) {
-    _classCallCheck(this, PhotosStage);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-    this.$dom = $dom;
-};
+var PhotosStage = (function () {
+    function PhotosStage($dom) {
+        _classCallCheck(this, PhotosStage);
+
+        this.$dom = $dom;
+        this.bindEvents();
+    }
+
+    // コレクションパネルのメニュー操作を管理するクラス
+
+    _createClass(PhotosStage, [{
+        key: 'bindEvents',
+        value: function bindEvents() {
+            // コレクションカードのタイトル領域をクリックされたとき
+            this.$dom.on('click', '', function (e) {});
+        }
+    }]);
+
+    return PhotosStage;
+})();
 
 var MenuBar = function MenuBar($dom) {
     _classCallCheck(this, MenuBar);
 
     this.$dom = $dom;
-};
+}
+
+// コレクションパネルを管理するクラス
+;
 
 var CollectionPanel = function CollectionPanel($window, $photosStage, $menuBar) {
     _classCallCheck(this, CollectionPanel);
--- www/src_collection_panel/CollectionPanel.js
+++ www/src_collection_panel/CollectionPanel.js
@@ -1,15 +1,27 @@
+// コレクションリストを管理するクラス
 class PhotosStage {
     constructor ($dom) {
         this.$dom = $dom;
+        this.bindEvents();
+    }
+
+    bindEvents () {
+        // コレクションカードのタイトル領域をクリックされたとき
+        this.$dom.on('click', '', e => {
+
+        });
     }
 }
 
+// コレクションパネルのメニュー操作を管理するクラス
 class MenuBar {
     constructor ($dom) {
         this.$dom = $dom;
     }
 }
 
+
+// コレクションパネルを管理するクラス
 class CollectionPanel {
     constructor ($window, $photosStage, $menuBar) {
         this.$window = $window;
